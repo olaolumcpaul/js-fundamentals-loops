cf1c6be97db7d3d7a2207ced9a851a1be283dac6
Olaoluwa Oyebode <olaolumcpaul@gmail.com>
2024-04-24 14:45:38 +0100

    completed exercise

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -5,12 +5,27 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 // to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
 let hasTen = false
 let indexOfTen = -1
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 10) {
+        hasTen = true
+        indexOfTen = i
+        break
+    }
+}
 
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 3 === 0) {
+        divisibleByThreeCount++
+    }
+}
 
 // 3. use a for loop to find the average of the numbers in the array
 let average = 0
+for (let i = 0; i < nums.length; i++) {
+    average += nums[i]
+}
 
 // 4. use a for loop to find the largest number in the array
 let largest = 0
--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -17,20 +17,45 @@ const deepThree = []
 // Your code here
 // }
 
+for (let i = START; i <= END; i++) {
+  simpleOne.length(i)
+    console.log(simpleOne)
+}
+
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
+for (let i = START; i <= END; i++) {
+    nestedOne.push([i])
+    console.log(nestedOne)
+}
 
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+for (let i = START; i <= END; i++) {
+    nestedTwo.push([i,i])
+    console.log(nestedTwo)
+}
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+for (let i = START; i <= END; i++) {
+    deepOne.push([i])
+    console.log(deepOne)
+}
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
+for (let i = START; i <= END; i++) {
+    deepTwo.push([i])
+    console.log(deepTwo)
+}
 
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+for (let i = START; i <= END; i++) {
+    deepThree.push([i])
+    console.log(deepThree)
+}
 
 module.exports = {
   START,
